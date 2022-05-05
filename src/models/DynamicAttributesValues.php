6b6bd69cac8c5a4d8c8257a22c5c99c450da271e
Pozitronik <antikillerxxl@gmail.com>
2022-05-05 16:39:36 +0300

    Новый метод для групповой установки атрибутов

--- src/models/DynamicAttributesValues.php
+++ src/models/DynamicAttributesValues.php
@@ -47,13 +47,35 @@ class DynamicAttributesValues extends DynamicAttributesValuesAR {
 		try {
 			$valueRecord = static::Upsert(compact('model_id', 'alias_id'));
 			if (ArrayHelper::getValue($valueRecord->attributes_values, $attribute_name) !== $attribute_value) {
-				$oldValues = $valueRecord->attributes_values;
+				$oldValues = $valueRecord->attributes_values;//изменять напрямую нельзя, поэтому промежуточная переменная
 				$oldValues[$attribute_name] = $attribute_value;
 				$valueRecord->attributes_values = $oldValues;
 				$valueRecord->save();
 			}
 			return $valueRecord;
 
+		} catch (Throwable $e) {
+			Yii::warning("Unable to update or insert table value: {$e->getMessage()}", __METHOD__);
+		}
+		return null;
+	}
+
+	/**
+	 * Изменяет значения динамических атрибутов
+	 * @param int $alias_id
+	 * @param int $model_id
+	 * @param array $attributes_values [attribute name => attribute value]
+	 * @return static|null
+	 * не проверялось
+	 */
+	public static function setAttributesValues(int $alias_id, int $model_id, array $attributes_values):?static {
+		try {
+			$valueRecord = static::Upsert(compact('model_id', 'alias_id'));
+			$oldValues = $valueRecord->attributes_values;
+			$oldValues = array_merge_recursive($oldValues, $attributes_values);
+			$valueRecord->attributes_values = $oldValues;
+			$valueRecord->save();
+			return $valueRecord;
 		} catch (Throwable $e) {
 			Yii::warning("Unable to update or insert table value: {$e->getMessage()}", __METHOD__);
 		}
