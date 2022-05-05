dbad30eb40831ea3d9dd8d179238e92870678492
Pozitronik <antikillerxxl@gmail.com>
2022-05-05 23:04:59 +0300

    limitFloatPrecision option

--- src/models/DynamicAttributesValues.php
+++ src/models/DynamicAttributesValues.php
@@ -14,8 +14,8 @@ use Yii;
  */
 class DynamicAttributesValues extends DynamicAttributesValuesAR {
 	/**
-	 * @var bool enable intermediate caching via Yii::$app->cache (must be configured in framework). Default option
-	 * value can be set in a module configuration, e.g.
+	 * @var bool enables a intermediate caching via Yii::$app->cache (must be configured in framework). The default option
+	 * value can be set in the module configuration, e.g.
 	 * ...
 	 * 'dynamic_attributes' => [
 	 *        'class' => DynamicAttributesModule::class,
@@ -25,7 +25,20 @@ class DynamicAttributesValues extends DynamicAttributesValuesAR {
 	 *        ],
 	 * ...
 	 */
-	public bool $cacheEnabled = true;
+	public bool $cacheEnabled = true;//todo: implement
+
+	/**
+	 * @var bool limit float attributes size to 64 bits (as php native floats).
+	 * By default, php uses a 64 bit floating-point precision (roughly 14 decimal digits, see [[https://www.php.net/manual/en/language.types.float.php]]).
+	 * But PostgreSQL may save float values with extended precision, that can cause problems with search, e.g.
+	 * php:5/7 => 0.71428571428571,
+	 * pgsql:5/7 => 0.7142857142857143
+	 *
+	 * >select * from table where float_column = 5/7 => nothing will be found
+	 *
+	 * This option forcibly restricts stored floats precision to 14 decimal digits, e.g. 0.7142857142857143 will be stored as 0.71428571428571
+	 */
+	public bool $limitFloatPrecision = true;
 
 	/**
 	 * {@inheritdoc}
@@ -33,6 +46,7 @@ class DynamicAttributesValues extends DynamicAttributesValuesAR {
 	public function init():void {
 		parent::init();
 		$this->cacheEnabled = DynamicAttributesModule::param('cacheEnabled', $this->cacheEnabled);
+		$this->limitFloatPrecision = DynamicAttributesModule::param('limitFloatPrecision', $this->limitFloatPrecision);
 	}
 
 	/**
@@ -44,6 +58,10 @@ class DynamicAttributesValues extends DynamicAttributesValuesAR {
 	 * @return DynamicAttributesValues|null
 	 */
 	public static function setAttributesValue(int $alias_id, int $model_id, string $attribute_name, mixed $attribute_value):?static {
+		if (is_float($attribute_value) && (new static())->limitFloatPrecision) {
+			$attribute_value = round($attribute_value,14);
+		}
+
 		try {
 			$valueRecord = static::Upsert(compact('model_id', 'alias_id'));
 			if (ArrayHelper::getValue($valueRecord->attributes_values, $attribute_name) !== $attribute_value) {
