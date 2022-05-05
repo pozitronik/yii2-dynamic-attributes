9f9662788837a1d317086826e852ccd575e01133
Pozitronik <antikillerxxl@gmail.com>
2022-05-05 19:47:10 +0300

    Адаптер адаптирует

--- src/models/adapters/ConditionAdapter.php
+++ src/models/adapters/ConditionAdapter.php
@@ -19,17 +19,19 @@ class ConditionAdapter {
 	 */
 	public static function adapt(array $condition):array {
 		if (isset($condition[0])) {//['operator', 'attribute_name', 'attribute_value']
-			$operator = $condition[0];
-			$attribute_name = $condition[1];
-			$attribute_value = $condition[2];
+			$operator = array_shift($condition);
+			$attribute_name = array_shift($condition);
+			$attribute_value = array_shift($condition);
 		} else { //['attribute_name' => 'attribute_value']
-			$operator = '=';
 			/** @var string $attribute_name */
 			$attribute_name = array_key_first($condition);
 			$attribute_value = $condition[$attribute_name];
+			$condition = [];
+			$operator = static::getOperator($attribute_value);
 		}
-
-		return [$operator, self::jsonFieldName(DynamicAttributesValues::tableName(), 'attributes_values', $attribute_name, DynamicAttributes::getType($attribute_value)), $attribute_value];
+		$adaptedExpression = [$operator, self::jsonFieldName(DynamicAttributesValues::tableName(), 'attributes_values', $attribute_name, DynamicAttributes::getType($attribute_value)), $attribute_value];
+		$adaptedExpression = array_merge($adaptedExpression, $condition);//В массиве могут остаться ещё какие-то параметры, например false в like - их просто добавим в адаптированное выражение
+		return $adaptedExpression;
 	}
 
 	/**
@@ -40,7 +42,7 @@ class ConditionAdapter {
 	 * @return string
 	 * @throws Exception
 	 */
-	public static function jsonFieldName(string $tableName, string $fieldName, string $jsonFieldName, int $fieldType):string {
+	public static function jsonFieldName(string $tableName, string $fieldName, string $jsonFieldName, ?int $fieldType):string {
 		$dataType = static::PHPTypeToPgSQLType($fieldType);
 		return "(\"".$tableName."\".\"".$fieldName."\"->>'".$jsonFieldName."')::{$dataType}";
 	}
@@ -50,7 +52,7 @@ class ConditionAdapter {
 	 * @param int $type
 	 * @return string
 	 */
-	public static function PHPTypeToPgSQLType(int $type):string {
+	public static function PHPTypeToPgSQLType(?int $type):string {
 		return match ($type) {
 			DynamicAttributes::TYPE_BOOL => 'boolean',
 			DynamicAttributes::TYPE_INT => 'int',
@@ -64,6 +66,16 @@ class ConditionAdapter {
 //			DynamicAttributes::TYPE_RESOURCE_CLOSED => '',
 			default => 'text'
 		};
+	}
+
+	/**
+	 * @param mixed $value
+	 * @return string
+	 */
+	private static function getOperator(mixed $value):string {
+		if (null === $value) return 'is';
+		if (is_array($value)) return 'in';
+		return '=';
 	}
 
 }
