a253e08f64196f4a7edb4d85ca9f539a536ab01e
Pozitronik <antikillerxxl@gmail.com>
2022-05-05 18:08:22 +0300

    Упростил адаптер

--- src/models/adapters/ConditionAdapter.php
+++ src/models/adapters/ConditionAdapter.php
@@ -3,33 +3,21 @@ declare(strict_types = 1);
 
 namespace pozitronik\dynamic_attributes\models\adapters;
 
-use pozitronik\dynamic_attributes\models\active_record\DynamicAttributesAliases;
+use Exception;
+use pozitronik\dynamic_attributes\models\DynamicAttributes;
 use pozitronik\dynamic_attributes\models\DynamicAttributesValues;
-use yii\base\BaseObject;
-use yii\db\ExpressionInterface;
 
 /**
  * Адаптируем условие для динамических параметров в ActiveQuery-условие
  */
-class ConditionAdapter extends BaseObject implements ExpressionInterface {
-
-	public array $expression = [];
-
-	/**
-	 * @param array $condition
-	 * @param array $config
-	 */
-	public function __construct(array $condition, array $config = []) {
-		$this->expression = $this->adaptExpression($condition);
-		parent::__construct($config);
-	}
+class ConditionAdapter {
 
 	/**
 	 * Превращает упрощённое условие выборки в массив для QueryBuilder
 	 * @param array $condition
 	 * @return array
 	 */
-	private function adaptExpression(array $condition):array {
+	public static function adapt(array $condition):array {
 		if (isset($condition[0])) {//['operator', 'attribute_name', 'attribute_value']
 			$operator = strtoupper(array_shift($condition));
 			$attribute_name = $condition[1];
@@ -41,7 +29,41 @@ class ConditionAdapter extends BaseObject implements ExpressionInterface {
 			$attribute_value = $condition[$attribute_name];
 		}
 
-		//вероятно, придётся изменить сериализацию на json $query->andWhere(['=', 'json', new ArrayExpression(['foo' => 'bar'])])
-		return ['and', [DynamicAttributesAliases::fieldName('attribute_name') => $attribute_name], [$operator, DynamicAttributesValues::fieldName('value'), $attribute_value]];
+		return [$operator, self::jsonFieldName(DynamicAttributesValues::tableName(), 'attributes_values', $attribute_name, DynamicAttributes::getType($attribute_value)), $attribute_value];
 	}
+
+	/**
+	 * MySQL и PostgreSQL по разному атрибутируют поля в json.
+	 * PGSQL ONLY!
+	 * @param string $fieldName
+	 * @param string $jsonFieldName
+	 * @return string
+	 * @throws Exception
+	 */
+	public static function jsonFieldName(string $tableName, string $fieldName, string $jsonFieldName, int $fieldType):string {
+		$dataType = static::PHPTypeToPgSQLType($fieldType);
+		return "(\"".$tableName."\".\"".$fieldName."\"->>'".$jsonFieldName."')::{$dataType}";
+	}
+
+	/**
+	 * Для типа данных php возвращает подходящий тип pgsql
+	 * @param int $type
+	 * @return string
+	 */
+	public static function PHPTypeToPgSQLType(int $type):string {
+		return match ($type) {
+			DynamicAttributes::TYPE_BOOL => 'boolean',
+			DynamicAttributes::TYPE_INT => 'int',
+			DynamicAttributes::TYPE_DOUBLE => 'float',
+			DynamicAttributes::TYPE_STRING => 'text',
+//			DynamicAttributes::TYPE_ARRAY => '', unsupported
+//			DynamicAttributes::TYPE_OBJECT => '',unsupported
+//			DynamicAttributes::TYPE_RESOURCE => '',unsupported
+//			DynamicAttributes::TYPE_NULL => '',//meaningless
+			DynamicAttributes::TYPE_UNKNOWN => 'text',//compatibility
+//			DynamicAttributes::TYPE_RESOURCE_CLOSED => '',
+			default => 'text'
+		};
+	}
+
 }
