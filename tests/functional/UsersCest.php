b93f55cba40f382bafd9c2bbb84356822d89ab95
Pozitronik <antikillerxxl@gmail.com>
2022-05-11 16:04:05 +0300

    Тест на динамические атрибуты

--- tests/functional/UsersCest.php
+++ tests/functional/UsersCest.php
@@ -2,6 +2,7 @@
 declare(strict_types = 1);
 
 use app\models\Users;
+use pozitronik\dynamic_attributes\models\DynamicAttributes;
 use yii\base\Exception as BaseException;
 
 /**
@@ -9,13 +10,16 @@ use yii\base\Exception as BaseException;
  */
 class UsersCest {
 
-
 	/**
 	 * @param FunctionalTester $I
 	 * @throws BaseException
 	 */
 	public function create(FunctionalTester $I):void {
+		DynamicAttributes::setClassAlias(Users::class, 'users');
 		$user = Users::CreateUser()->saveAndReturn();
+		$user->addDynamicAttribute('weight', DynamicAttributes::TYPE_INT);
+		$user->addDynamicAttribute('sex', DynamicAttributes::TYPE_BOOL);
+		$user->addDynamicAttribute('кириллическое имя', DynamicAttributes::TYPE_STRING);
 
 		$I->amLoggedInAs($user);
 		$I->amOnRoute('users/create');
@@ -24,7 +28,10 @@ class UsersCest {
 			'Users' => [
 				'username' => 'Test Successful',
 				'login' => 'test_user_2',
-				'password' => '123'
+				'password' => '123',
+				'da2' => 'abyrvalg',//кириллическое имя
+				'da1' => true,//sex,
+				'weight' => 42//weight
 			]
 		]);
 		$I->seeResponseCodeIs(200);
@@ -35,5 +42,8 @@ class UsersCest {
 		$I->assertEquals(2, $model->id);
 		$I->assertEquals('test_user_2', $model->login);
 		$I->assertEquals('123', $model->password);
+		$I->assertEquals('abyrvalg', $model->{'кириллическое имя'});
+		$I->assertEquals(true, $model->sex);
+		$I->assertEquals(42, $model->weight);
 	}
 }
