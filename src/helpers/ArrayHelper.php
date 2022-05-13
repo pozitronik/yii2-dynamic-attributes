b9af8c0e00e7b2c285ae5b27e63304fbc5a7437c
Pozitronik <antikillerxxl@gmail.com>
2022-05-13 21:18:40 +0300

    #3 Сравнение массивов переписано так, чтобы float в значениях сравнивались корректно

--- src/helpers/ArrayHelper.php
+++ src/helpers/ArrayHelper.php
@@ -14,6 +14,7 @@ class ArrayHelper extends VendorArrayHelper {
 	public const FLAG_COMPARE_KEYS = 1;//наборы ключей должны совпадать
 	public const FLAG_COMPARE_VALUES = 2;//наборы значений должны совпадать
 	public const FLAG_COMPARE_KEY_VALUES_PAIRS = 4;//наборы ключ-значение должны совпадать
+	public const FLOAT_DELTA = 1.0E-13;//Дельта сравнения двух чисел с плавающей точкой внутри PHP
 
 	/**
 	 * Сравнивает два массива между собой по наборам данных (с учётом вложенности).
@@ -26,14 +27,14 @@ class ArrayHelper extends VendorArrayHelper {
 	public static function isEqual(array|Traversable $array_one, array|Traversable $array_two, int $flags = 7):bool {
 		if (count($array_one) !== count($array_two)) return false;
 		foreach ($array_one as $a1key => $a1value) {
-			if (($flags & self::FLAG_COMPARE_KEYS) && !array_key_exists($a1key, $array_two)) {//разница по ключам
+			if (($flags & self::FLAG_COMPARE_KEYS) && !static::keyExists($a1key, $array_two)) {//разница по ключам
 				return false;
 			}
-			if (($flags & self::FLAG_COMPARE_VALUES) && !in_array($a1value, $array_two, true)) {//разница по значениям
+			if (($flags & self::FLAG_COMPARE_VALUES) && !static::isInWithFloatDelta($a1value, $array_two, true)) {//разница по значениям
 				return false;
 			}
 			if ($flags & self::FLAG_COMPARE_KEY_VALUES_PAIRS) {
-				if (!array_key_exists($a1key, $array_two) || $a1value !== $array_two[$a1key]) {
+				if (!static::keyExists($a1key, $array_two) || !static::isEquals($a1value, $array_two[$a1key])) {
 					return false;
 				}
 			}
@@ -44,4 +45,53 @@ class ArrayHelper extends VendorArrayHelper {
 
 		return true;
 	}
+
+	/**
+	 * @param mixed $scalarOne
+	 * @param mixed $scalarTwo
+	 * @return bool
+	 */
+	private static function isEquals(mixed $scalarOne, mixed $scalarTwo):bool {
+		if (is_float($scalarOne) && is_float($scalarTwo)) return static::isFloatEquals($scalarOne, $scalarTwo);
+		return $scalarOne === $scalarTwo;
+	}
+
+	/**
+	 * @param mixed $floatOne
+	 * @param mixed $floatTwo
+	 * @param float $delta
+	 * @return bool
+	 */
+	public static function isFloatEquals(mixed $floatOne, mixed $floatTwo, float $delta = self::FLOAT_DELTA):bool {
+		if (is_infinite($floatTwo) && is_infinite($floatOne)) {
+			return true;
+		}
+
+		if ((is_infinite($floatTwo) xor is_infinite($floatOne)) ||
+			(is_nan($floatTwo) || is_nan($floatOne)) ||
+			abs($floatTwo - $floatOne) > $delta) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Функция аналогична static::isIn() но для поиска float-значений учитывает дельту PHP
+	 * @param mixed $needle
+	 * @param array|Traversable $haystack
+	 * @param bool $strict
+	 * @return bool
+	 */
+	public static function isInWithFloatDelta(mixed $needle, array|Traversable $haystack, bool $strict = false):bool {
+		if (is_float($needle)) {
+			foreach ($haystack as $value) {
+				if (is_float($value) && static::isFloatEquals($needle, $value)) {
+					return true;
+				}
+			}
+			return false;
+		}
+		return static::isIn($needle, $haystack, $strict);
+	}
+
 }
