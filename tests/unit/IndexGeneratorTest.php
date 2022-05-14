644cf76a900d6a6d4e7f1b59695d84c00a789bd7
Pozitronik <antikillerxxl@gmail.com>
2022-05-14 13:16:53 +0300

    fixed: не передавалось количество создаваемых записей

--- tests/unit/IndexGeneratorTest.php
+++ tests/unit/IndexGeneratorTest.php
@@ -38,11 +38,11 @@ class IndexGeneratorTest extends Unit {
 	 * Количество записей, на которых будут прогоняться тесты. Для теста работоспособности можно установить
 	 * минимальное значение, для тестов производительности - повысить его.
 	 */
-	public const TESTING_RECORDS_CNT = 10000;
+	public const TESTING_RECORDS_CNT = 100;
 	/**
 	 * Количество повторяющихся поисковых запросов на каждый тип атрибута.
 	 */
-	public const TESTING_SEARCH_REPEATS = 100;
+	public const TESTING_SEARCH_REPEATS = 1;
 
 	/**
 	 * @inheritDoc
@@ -63,6 +63,52 @@ class IndexGeneratorTest extends Unit {
 		 * Динамически регистрируем алиас класса.
 		 */
 		DynamicAttributes::setClassAlias(Users::class, 'users');
+	}
+
+	/**
+	 * Тестирует создание индексированных полей с последующим их заполнением
+	 * @return void
+	 * @throws Throwable
+	 */
+	public function testIndexCreation():void {
+		foreach (static::DYNAMIC_ATTRIBUTES as $attributeName => $attributeType) {
+			DynamicAttributes::ensureAttribute(Users::class, $attributeName, $attributeType, true);
+		}
+
+		$startTime = microtime(true);
+		static::fillData(self::TESTING_RECORDS_CNT);
+		$finalTime = microtime(true) - $startTime;
+		Console::output(sprintf("Write %d records to %s table take %s", self::TESTING_RECORDS_CNT, Console::renderColoredString("%rindexed%n"), Console::renderColoredString("%g{$finalTime} sec%n")));
+
+	}
+
+	/**
+	 * Тестирует создание неиндексированных полей с последующим их заполнением и генерацией индексов
+	 * @return void
+	 * @throws Throwable
+	 * @throws DbException
+	 */
+	public function testIndexGeneration():void {
+		foreach (static::DYNAMIC_ATTRIBUTES as $attributeName => $attributeType) {
+			DynamicAttributes::ensureAttribute(Users::class, $attributeName, $attributeType, false);
+		}
+		$startTime = microtime(true);
+		static::fillData(self::TESTING_RECORDS_CNT);
+		$finalTime = microtime(true) - $startTime;
+		Console::output(sprintf("Write %d records to %s table take %s", self::TESTING_RECORDS_CNT, Console::renderColoredString("%runindexed%n"), Console::renderColoredString("%g{$finalTime} sec%n")));
+		/*измеряем скорость поиска без индексов*/
+		Console::output(Console::renderColoredString("%gSearch time tests on unindexed table...%n"));
+		static::measureSearchTime(self::TESTING_SEARCH_REPEATS);
+
+		$startTime = microtime(true);
+		foreach (static::DYNAMIC_ATTRIBUTES as $attributeName => $attributeType) {
+			DynamicAttributes::indexAttribute(Users::class, $attributeName, $attributeType);
+		}
+		$finalTime = microtime(true) - $startTime;
+		Console::output(sprintf("Index creation on %d records take %s", self::TESTING_RECORDS_CNT, Console::renderColoredString("%g{$finalTime} sec%n")));
+		/*измеряем скорость поиска с индексами*/
+		Console::output(Console::renderColoredString("%gSearch time tests on indexed table...%n"));
+		static::measureSearchTime(self::TESTING_SEARCH_REPEATS);
 	}
 
 	/**
@@ -98,54 +144,6 @@ class IndexGeneratorTest extends Unit {
 			Console::updateProgress($rowCount, $rowsCount);
 		}
 		Console::endProgress();
-	}
-
-	/**
-	 * Тестирует создание индексированных полей с последующим их заполнением
-	 * @return void
-	 * @throws Throwable
-	 * @skip
-	 */
-	public function testIndexCreation():void {
-		foreach (static::DYNAMIC_ATTRIBUTES as $attributeName => $attributeType) {
-			DynamicAttributes::ensureAttribute(Users::class, $attributeName, $attributeType, true);
-		}
-
-		$startTime = microtime(true);
-		static::fillData(self::TESTING_RECORDS_CNT);
-		$finalTime = microtime(true) - $startTime;
-		Console::output(sprintf("Write %d records to %s table take %s", self::TESTING_RECORDS_CNT, Console::renderColoredString("%rindexed%n"), Console::renderColoredString("%g{$finalTime} sec%n")));
-
-	}
-
-	/**
-	 * Тестирует создание неиндексированных полей с последующим их заполнением и генерацией индексов
-	 * @return void
-	 * @throws Throwable
-	 * @throws DbException
-	 * @skip
-	 */
-	public function testIndexGeneration():void {
-		foreach (static::DYNAMIC_ATTRIBUTES as $attributeName => $attributeType) {
-			DynamicAttributes::ensureAttribute(Users::class, $attributeName, $attributeType, false);
-		}
-		$startTime = microtime(true);
-		static::fillData();
-		$finalTime = microtime(true) - $startTime;
-		Console::output(sprintf("Write %d records to %s table take %s", self::TESTING_RECORDS_CNT, Console::renderColoredString("%runindexed%n"), Console::renderColoredString("%g{$finalTime} sec%n")));
-		/*измеряем скорость поиска без индексов*/
-		Console::output(Console::renderColoredString("%gSearch time tests on unindexed table...%n"));
-		static::measureSearchTime(self::TESTING_SEARCH_REPEATS);
-
-		$startTime = microtime(true);
-		foreach (static::DYNAMIC_ATTRIBUTES as $attributeName => $attributeType) {
-			DynamicAttributes::indexAttribute(Users::class, $attributeName, $attributeType);
-		}
-		$finalTime = microtime(true) - $startTime;
-		Console::output(sprintf("Index creation on %d records take %s", self::TESTING_RECORDS_CNT, Console::renderColoredString("%g{$finalTime} sec%n")));
-		/*измеряем скорость поиска с индексами*/
-		Console::output(Console::renderColoredString("%gSearch time tests on indexed table...%n"));
-		static::measureSearchTime(self::TESTING_SEARCH_REPEATS);
 	}
 
 	/**
